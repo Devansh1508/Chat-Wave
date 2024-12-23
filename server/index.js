554d386c0b8ab092d37a7425ebf295bb189e982c
Devansh <devanshpratap1500@gmail.com>
2024-12-23 17:58:12 +0530

    error in index.js resolved

--- server/index.js
+++ server/index.js
@@ -27,27 +27,3 @@ app.get('/user', userRoute);
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-const setUpSocket = require('./sockets/socket');
-const express = require('express');
-const http = require('http');
-const app = express();
-
-require('dotenv').config();
-const PORT = process.env.PORT || 5000;
-
-// cors middleware
-const cors = require('cors');
-app.use(cors());
-
-// sockets 
-const server = http.createServer(app);
-setUpSocket(server);
-
-app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index?.html');
-});
-
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
